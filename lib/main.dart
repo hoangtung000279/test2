f82f4a7616122d78e9fb8b28e7995364e899a645
Tung <tungb2110981@student.ctu.edu.vn>
2025-06-02 11:48:34 +0700

    Thêm dòng print vào main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -65,6 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+    print("aaaaaaaaa");
   }
 
   @override
@@ -103,13 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: <Widget>[const Text('You have pushed the button this many times:'), Text('$_counter', style: Theme.of(context).textTheme.headlineMedium)],
         ),
       ),
       floatingActionButton: FloatingActionButton(
